4038cc0344dfd024537a8dbbcb275aa19ccf0d29
Fabioribeiro7 <103196452+Fabioribeiro7@users.noreply.github.com>
2022-05-05 11:13:35 +0200

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -12,7 +12,7 @@ import json
 # save the positive words into a list called p_list
 with open('positive.txt') as f:
     p_txt = f.read()
-    p_txt = re.sub('[,\.()":;!@#$%^&*\d]|\'s|\'', '', p_txt)
+    
     p_list = p_txt.replace('\n',' ').replace('  ',' ').lower().split(' ')
     # test if cool is in the list
     print('cool is in the postive list: ', 'cool' in p_list)
@@ -22,18 +22,11 @@ with open('positive.txt') as f:
 # save the negative words into a list called n_list
 with open('negative.txt') as f:
     n_txt = f.read()
-    n_txt = re.sub('[,\.()":;!@#$%^&*\d]|\'s|\'', '', n_txt)
+    
     n_list = n_txt.replace('\n',' ').replace('  ',' ').lower().split(' ')
-    # test if abrade is in the list
-    print('abrade is in the negative list: ', 'abrade' in n_list)
-    # test if cool is in the list
-    print 'cool is in the negative list: ', 'cool' in p_list 
+   
 
 # process the tweets
-with open('negative2.rtf') as f:
-    txt = f.read()
-    txt = re.sub('[,\.()":;!@#$%^&*\d]|\'s|\'', '', txt)
-    word_list = txt.replace('\n',' ').replace('  ',' ').lower().split(' ')
     
     word_list1
     word_list2 = []
